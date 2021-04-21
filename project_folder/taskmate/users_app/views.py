1746f66feb4c6ab9d65798608cbd72b2b5a5f233
Jan Błażejczyk <JB.Blazejczyk@gmail.com>
2021-04-21 20:18:43 +0200

    editing user creation view

--- project_folder/taskmate/users_app/views.py
+++ project_folder/taskmate/users_app/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from .forms import CustomUserCreationForm
 from django.contrib import messages
 
 
 def register(request):
     if request.method == 'POST':
-        register_form = UserCreationForm(request.POST)
+        register_form = CustomUserCreationForm(request.POST)
         if register_form.is_valid():
             register_form.save()
             messages.success(
@@ -13,6 +13,6 @@ def register(request):
             return redirect('register')
     else:
         # our form which is the instance of custom form class
-        register_form = UserCreationForm()
+        register_form = CustomUserCreationForm()
 
     return render(request, "register.html", {'register_form': register_form})
