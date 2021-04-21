edbed06521af54decbd816a81baf0954d3ff2245
Jan Błażejczyk <JB.Blazejczyk@gmail.com>
2021-04-21 20:09:10 +0200

    adding a registration form

--- project_folder/taskmate/users_app/views.py
+++ project_folder/taskmate/users_app/views.py
@@ -1,11 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 
 def register(request):
-    # our form which is the instance of custom form class
-    register_form = UserCreationForm()
-    context = {
-        'register_form': register_form
-    }
-    return render(request, "register.html", context)
+    if request.method == 'POST':
+        register_form = UserCreationForm(request.POST)
+        if register_form.is_valid():
+            register_form.save()
+            messages.success(
+                request, "New User Account Created, Log In to Get Started")
+            return redirect('register')
+    else:
+        # our form which is the instance of custom form class
+        register_form = UserCreationForm()
+
+    return render(request, "register.html", {'register_form': register_form})
